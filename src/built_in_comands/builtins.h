1201bd7c048fa952612edf2eff38a7abb1d94367
UlianaNad <unadorozhna1@gmail.com>
2025-09-24 07:52:33 +0300

    added finction export, but it is not working yet

--- src/built_in_comands/builtins.h
+++ src/built_in_comands/builtins.h
@@ -17,7 +17,7 @@ typedef struct s_parser
 int	builtin_echo(int argc, char **argv, char **envp);
 // int builtin_cd(t_shell_data *data, char **args);
 // int builtin_pwd(t_shell_data *data);
-// int builtin_export(t_shell_data *data, char **args);
+int builtin_export(t_shell_data *data, char **args);
 // int builtin_unset(t_shell_data *data, char **args);
 // int builtin_env(t_shell_data *data);
 // int builtin_exit(t_shell_data *data, char **args);
@@ -26,3 +26,4 @@ int is_builtin(char *cmd);
 int exec_builtin(t_shell_data *data, char **args);
 
 #endif
+
